e30a5f2e513998dd6733fe1d3a8c0bc7255132db
Steinn Steinsen <steinnsteinsen@gmail.com>
2012-12-30 15:56:57 +0000

    format package list

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -40,13 +40,29 @@
 (package-initialize)
 
 (defvar prelude-packages
-  '(ace-jump-mode ack-and-a-half diminish elisp-slime-nav
-    expand-region flycheck gist
-    git-commit-mode gitconfig-mode gitignore-mode
-    guru-mode helm helm-projectile
-    magit melpa
-    rainbow-mode solarized-theme undo-tree
-    volatile-highlights yasnippet zenburn-theme)
+  '(
+    ace-jump-mode
+    ack-and-a-half
+    diminish
+    elisp-slime-nav
+    expand-region
+    flycheck
+    gist
+    git-commit-mode
+    gitconfig-mode
+    gitignore-mode
+    guru-mode
+    helm
+    helm-projectile
+    magit
+    melpa
+    rainbow-mode
+    solarized-theme
+    undo-tree
+    volatile-highlights
+    yasnippet
+    zenburn-theme
+    )
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
