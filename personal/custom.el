8c4ec4b01d83f2d011e26f3c3719e5f83f468852
Steinn Steinsen <steinnsteinsen@gmail.com>
2013-01-03 23:59:20 +0000

    change flycheck warning and error faces

--- personal/custom.el
+++ personal/custom.el
@@ -20,4 +20,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-error-face ((t (:underline "red"))))
+ '(flycheck-warning-face ((t (:underline "blue")))))
