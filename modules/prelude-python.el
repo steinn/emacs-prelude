41bae9a6954b3b0e0d6209edee45b20b5da8bbac
Steinn Steinsen <steinnsteinsen@gmail.com>
2013-01-17 09:42:41 +0000

    setup python.el to use IPython

--- modules/prelude-python.el
+++ modules/prelude-python.el
@@ -40,11 +40,24 @@
 
 ;; Setup python-mode
 (autoload 'python-mode "python" "Python Mode." t)
-(setq python-shell-interpreter "ipython")
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 ;; Setup jedi (auto-completion)
 (setq jedi:setup-keys t)
 (autoload 'jedi:setup "jedi" nil t)
+
+
 
 (defun prelude-python-mode-defaults ()
   (run-hooks 'prelude-prog-mode-hook) ;; run manually; not derived from prog-mode
