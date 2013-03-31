223c9313a5574c753f4dbfc3a6ee5cc9276e928e
Steinn Steinsen <steinnsteinsen@gmail.com>
2013-03-31 10:51:50 +0000

    custom.el

--- personal/custom.el
+++ personal/custom.el
@@ -5,22 +5,21 @@
  ;; If there is more than one, they won't work right.
  '(abbrev-file-name "~/.emacs.d/personal/abbrev_defs")
  '(ac-comphist-file "/home/steinn/.emacs.d/personal/ac-comphist.dat")
+ '(ansi-color-names-vector ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(ansi-term-color-vector ["#586e75" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#002b36"])
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(dired-listing-switches "-al --group-directories-first")
  '(erc-away-nickname nil)
  '(erc-nick "joggz")
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
  '(indicate-buffer-boundaries (quote left))
  '(indicate-empty-lines t)
  '(jedi:server-args (quote ("--sys-path" "/home/steinn/work/birdcore")))
+ '(jedi:server-command (quote ("python2" "/home/steinn/.emacs.d/elpa/jedi-20130223.4/jediepcserver.py")))
  '(safe-local-variable-values (quote ((python-run-file . "/home/steinn/work/birdcore/special/steinn/emacs_shell.py") (python-run-file . "run.py") (python-shell-interpreter . "python") (python-shell-interpreter . "ipython") (python-shell-interpreter "ipython") (virtualenv-workon . "birdcore") (eval when (fboundp (quote rainbow-mode)) (rainbow-mode 1)))))
  '(visible-mark-inhibit-trailing-overlay t))
-
-(eval-after-load  "zenburn" (custom-theme-set-faces
-                             `zenburn
-                             `(flymake-infoline ((t (:underline "green"))))
-                             `(flymake-warnline ((t (:underline "blue"))))
-                             '(flycheck-error-face ((t (:underline "red"))))
-                             '(flycheck-warning-face ((t (:underline "blue"))))
-                             ))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
